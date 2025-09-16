239593be48003a53c988d1a0b0c2d09901b5af5e
Facundo Azcue <fazcue@gmail.com>
2025-09-15 22:14:00 -0300

    Se corrigió texto dedicado a bitácora

--- js/data/bitacora.js
+++ js/data/bitacora.js
@@ -1,14 +1,14 @@
 export const decisiones = [
 	'Bitácora: se eligió un diseño simple y minimalista para mejorar la legibilidad.',
-	'Bitácora: se utilizo flexbox para la disposición de los elementos.',
+	'Bitácora: se utilizó flexbox para la disposición de los elementos.',
 	'Bitácora: se cargan dinámicamente listados de decisiones, dificultades y cambios.',
 	'Navegación: se definió un menú de navegación claro con enlaces a las secciones principales (Portada, Integrantes y Bitácora) para facilitar la navegación del usuario.',
 	'Navegación: se implementó un menú hamburguesa para mejorar la experiencia en dispositivos móviles.',
 ]
 
 export const dificultades = [
-	'Bitácora: inicialmente los ítems de la lista aparecían desordenados → se resolvió con estilos de lista y bordes.',
-	'Bitácora: se presentó dificultad para mostrar/ocultar secciones dinámicamente → se solucionó con JavaScript y toggle de clases.',
+	'Bitácora: inicialmente los ítems de la lista aparecían desordenados. Se resolvió con estilos de lista y bordes.',
+	'Bitácora: se presentó dificultad para mostrar/ocultar secciones dinámicamente. Se solucionó con JavaScript y toggle de clases.',
 	'Navegación: se tuvo que ajustar el diseño del menú hamburguesa para que se oculte en pantalla completa y se muestre en el extremo superior derecho en pantallas pequeñas y no junto al logo de la página.',
 	'Navegación: al implementar los Dropdowns, se tuvo que ajustar el comportamiento para que se abran al pasar el mouse y se cierren al salir',
 ]
